ad589851ff450f25abf55d2add5f010170dfe49f
brunombs <brunozs@msn.com>
2024-08-20 04:27:48 -0300

    improving sum

--- 00-Java-Basic/Sum/Sum.java
+++ 00-Java-Basic/Sum/Sum.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Input the first number: ");
+            int n1 = input.nextInt();
 
-        System.out.println("Input the first number: ");
-        int n1 = input.nextInt();
+            System.out.println("Input the second number: ");
+            int n2 = input.nextInt();
 
-        System.out.println("Input the second number: ");
-        int n2 = input.nextInt();
-
-        int sum = n1 + n2;
-        System.out.println("The sum of " + n1 + "+" + n2 + "=" + sum);
+            int sum = n1 + n2;
+            System.out.println("The sum of " + n1 + "+" + n2 + "=" + sum);
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter valid integers.");
+        }
     }
 }
