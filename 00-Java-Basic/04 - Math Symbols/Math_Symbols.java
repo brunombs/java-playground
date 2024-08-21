4375818299c1414bd728ad9cf5a4d2c009213605
brunombs <brunozs@msn.com>
2024-08-21 01:18:01 -0300

    adding exercise

---
+++ 00-Java-Basic/04 - Math Symbols/Math_Symbols.java
@@ -0,0 +1,7 @@
+// Write a Java program to print the results of the following operations.
+
+public class Math_Symbols {
+    public static void main(String[] args) {
+        System.out.println("eae");
+    }
+}
