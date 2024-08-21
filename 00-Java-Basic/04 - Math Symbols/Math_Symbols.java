41ec1b0bf8f099f1dd820fdb3ca4289565980095
brunombs <brunozs@msn.com>
2024-08-21 01:25:15 -0300

    improving one exercise

--- 00-Java-Basic/04 - Math Symbols/Math_Symbols.java
+++ 00-Java-Basic/04 - Math Symbols/Math_Symbols.java
@@ -2,6 +2,11 @@
 
 public class Math_Symbols {
     public static void main(String[] args) {
-        System.out.println("eae");
+        int a = -5 + 8 * 6;
+        int b = (55+9) % 9;
+        int c = 20 + -3 * 5 / 8;
+        int d = 5 + 15 / 3 * 2 - 8 % 3;
+
+        System.out.print(a + "\n" + b + "\n" + c + "\n" + d);
     }
 }
