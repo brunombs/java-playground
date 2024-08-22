a1c116b946dd2541001db84929fc82bbbedf8f62
brunombs <brunozs@msn.com>
2024-08-22 04:16:45 -0300

    adding description to exercise

--- Random Codes/01 - Sum From 1 to 100/Sum1to100.java
+++ Random Codes/01 - Sum From 1 to 100/Sum1to100.java
@@ -1,3 +1,5 @@
+// This Java program calculates and displays the sum of all integers from 1 to 100 using a for loop.
+
 public class Sum1to100 {
     public static void main(String[] args) {
         int sum = 0;
