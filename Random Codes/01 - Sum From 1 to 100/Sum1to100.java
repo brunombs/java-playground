3d4aff7e31aa93b032285aba9c226bca542f8cff
brunombs <brunozs@msn.com>
2024-08-22 04:15:20 -0300

    adding exercise

---
+++ Random Codes/01 - Sum From 1 to 100/Sum1to100.java
@@ -0,0 +1,9 @@
+public class Sum1to100 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int num = 1; num <=100; num++){
+            sum += num;
+        }
+        System.out.println("The sum from 1 to 100 is: " + sum);
+    }
+}
