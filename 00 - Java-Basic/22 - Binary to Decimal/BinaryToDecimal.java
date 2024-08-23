23579162fe1f38bba8eee95ab9c42e21ebca241b
brunombs <brunozs@msn.com>
2024-08-23 18:53:38 -0300

    adding exercise

---
+++ 00 - Java-Basic/22 - Binary to Decimal/BinaryToDecimal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a binary number: ");
+        String binary = input.nextLine();
+        int decimal = Integer.parseInt(binary, 2);
+        System.out.println("The binary number is: " + binary + " and the decimal number is: " + decimal);
+    }
+}
