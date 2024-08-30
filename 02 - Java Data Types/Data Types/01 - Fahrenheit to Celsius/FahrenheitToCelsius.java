69af15f88f27224048c19d20197786b78c29c7e9
brunombs <brunozs@msn.com>
2024-08-30 03:03:42 -0300

    adding exercise

---
+++ 02 - Java Data Types/Data Types/01 - Fahrenheit to Celsius/FahrenheitToCelsius.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the temperature in Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        System.out.printf("Fahrenheit: %.2f and Celsius: %.2f%n", fahrenheit, celsius);
+    }
+}
