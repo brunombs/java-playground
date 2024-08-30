95c08f620387e8ceed88e01db2b8c0b2c014bf89
brunombs <brunozs@msn.com>
2024-08-29 23:59:29 -0300

    improving exercise

--- Random Codes/14 - Basic/BasicAndRandom.java
+++ Random Codes/14 - Basic/BasicAndRandom.java
@@ -1,12 +1,41 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BasicAndRandom {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number between 1 and 100: ");
-        int firstNumber = input.nextInt();
-        System.out.print("Enter a number between 1 and 100: ");
-        int secondNumber = input.nextInt();
+        int firstNumber;
+        int secondNumber;
+
+        while (true) {
+            System.out.print("Enter a number between 1 and 100: ");
+            try {
+                firstNumber = input.nextInt();
+                if (firstNumber >= 1 && firstNumber <= 100) {
+                    break;
+                } else {
+                    System.out.println("Please enter a number between 1 and 100.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+                input.next();
+            }
+        }
+
+        while (true) {
+            System.out.print("Enter another number between 1 and 100: ");
+            try {
+                secondNumber = input.nextInt();
+                if (secondNumber >= 1 && secondNumber <= 100) {
+                    break;
+                } else {
+                    System.out.println("Please enter a number between 1 and 100.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+                input.next();
+            }
+        }
 
         if (firstNumber > secondNumber) {
             System.out.println(firstNumber + " is higher than " + secondNumber);
