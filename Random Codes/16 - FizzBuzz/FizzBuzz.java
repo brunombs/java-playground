65562a9e078ff768f1b7840aa52c7fde12b3a7ec
brunombs <brunozs@msn.com>
2024-09-04 23:56:41 -0300

    improving exercise

--- Random Codes/16 - FizzBuzz/FizzBuzz.java
+++ Random Codes/16 - FizzBuzz/FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        int number = 0;
+        int number = 1;
         while(number<=100){
             if(number % 3 == 0 && number % 5 == 0){
                 System.out.println("FizzBuzz");
