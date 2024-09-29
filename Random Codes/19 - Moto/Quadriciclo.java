27f06bb8b0b553f8a3a9af58b8925e0337d8f2a0
brunombs <brunozs@msn.com>
2024-09-29 00:21:48 -0300

    adding exercise

---
+++ Random Codes/19 - Moto/Quadriciclo.java
@@ -0,0 +1,25 @@
+public class Quadriciclo {
+    private String cor;
+    private int ano;
+
+    public Quadriciclo(String cor, int ano) {
+        this.cor = cor;
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
