ec3813775ec5d25f635b895ccd68037874a694bb
brunombs <brunozs@msn.com>
2024-09-29 03:35:38 -0300

    improving exercise

--- 05 - Object Oriented Programming/Java Static Members/01 - Counter/Counter.java
+++ 05 - Object Oriented Programming/Java Static Members/01 - Counter/Counter.java
@@ -1,3 +1,5 @@
+// Write a Java program to create a class called "Counter" with a static variable count. Implement a constructor that increments count every time an object is created. Print the value of count after creating several objects.
+
 public class Counter {
     private static int count = 0;
 
