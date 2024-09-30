1660bbf28f7648c2a45d7aee8a70dce094e840f1
brunombs <brunozs@msn.com>
2024-09-30 18:12:39 -0300

    improving exercise

--- Random Codes/24 - Student v3/Studentv3.java
+++ Random Codes/24 - Student v3/Studentv3.java
@@ -1,37 +1,37 @@
 public class Studentv3 {
-    private final String nome;
-    private double nota1;
-    private double nota2;
-    private double notaFinal;
+    private final String name;
+    private double grade1;
+    private double grade2;
+    private double finalGrade;
 
-    public Studentv3(String nome) {
-        this.nome = nome;
+    public Studentv3(String name) {
+        this.name = name;
     }
 
-    public void adicionarNota1(double nota1) {
-        this.nota1 += nota1;
+    public void addGrade1(double grade1) {
+        this.grade1 += grade1;
     }
 
-    public void adicionarNota2(double nota2) {
-        this.nota2 += nota2;
+    public void addGrade2(double grade2) {
+        this.grade2 += grade2;
     }
 
-    public void adicionarNotaFinal() {
-        this.notaFinal += (nota1 + nota2)/2;
+    public void calculateFinalGrade() {
+        this.finalGrade += (grade1 + grade2) / 2;
     }
 
-    public void details(){
-        System.out.println("Nome: " + nome);
-        System.out.println("Nota 1ª prova: " + nota1);
-        System.out.println("Nota 2ª prova: " + nota2);
-        System.out.println("Média: " + notaFinal);
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Grade for 1st exam: " + grade1);
+        System.out.println("Grade for 2nd exam: " + grade2);
+        System.out.println("Average: " + finalGrade);
     }
 
     public static void main(String[] args) {
         Studentv3 student1 = new Studentv3("Bruno");
-        student1.adicionarNota1(9.5);
-        student1.adicionarNota2(8.7);
-        student1.adicionarNotaFinal();
-        student1.details();
+        student1.addGrade1(9.5);
+        student1.addGrade2(8.7);
+        student1.calculateFinalGrade();
+        student1.displayDetails();
     }
 }
