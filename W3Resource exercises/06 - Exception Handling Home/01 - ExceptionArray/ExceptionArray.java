1374dfcd8b4035bdd030f1b6bd08c6f751676968
brunombs <brunozs@msn.com>
2024-10-21 23:38:26 -0300

    adding exercise

---
+++ W3Resource exercises/06 - Exception Handling Home/01 - ExceptionArray/ExceptionArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExceptionArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Choose a random number: ");
+        int number = input.nextInt();
+
+        try {
+            int[] array = {1, 2, 3};
+            System.out.println(array[number]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
