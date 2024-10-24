459a00bdea40f7b8a998bb5084ae1e59e4d994b1
brunombs <brunozs@msn.com>
2024-10-23 23:32:19 -0300

    adding exercise

---
+++ W3Resource exercises/05 - Object Oriented Programming/04 - Java Inheritance/02 - Vehicle/Vehicle.java
@@ -0,0 +1,7 @@
+// Write a Java program to create a class called Vehicle with a method called drive(). Create a subclass called Car that overrides the drive() method to print "Repairing a car".
+
+public class Vehicle {
+    public void drive(){
+        System.out.println("Driving vehicle");
+    }
+}
---
+++ W3Resource exercises/05 - Object Oriented Programming/04 - Java Inheritance/02 - Vehicle/VehicleMain.java
@@ -0,0 +1,8 @@
+public class VehicleMain {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        Vehicle repair = new CarInheritance();
+        vehicle.drive();
+        repair.drive();
+    }
+}
