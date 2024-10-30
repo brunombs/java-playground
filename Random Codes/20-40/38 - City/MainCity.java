6252d1fc494afd0b984aa73b7aec4297525e5bfa
brunombs <brunozs@msn.com>
2024-10-30 17:28:59 -0300

    improving exercise

--- Random Codes/20-40/38 - City/MainCity.java
+++ Random Codes/20-40/38 - City/MainCity.java
@@ -1,15 +1,13 @@
-public class MainCity {
-    public static void main(String[] args) {
-        City Salvador = new City("Salvador", 2886698, 693.8, "Bahia", true);
-        City SP = new City("São Paulo", 12325232, 1521, "São Paulo", true);
-        City Curitiba = new City("Curitiba", 1948626, 4349, "Paraná", true);
-        City Campinas = new City("Campinas", 1223237, 7953, "São Paulo", false);
+public static void main(String[] args) {
+    City Salvador = new City("Salvador", 2886698, 693.8, "Bahia", true);
+    City SP = new City("São Paulo", 12325232, 1521, "São Paulo", true);
+    City Curitiba = new City("Curitiba", 1948626, 4349, "Paraná", true);
+    City Campinas = new City("Campinas", 1223237, 7953, "São Paulo", false);
 
-        City[] cities = {Salvador, SP, Curitiba, Campinas};
+    City[] cities = {Salvador, SP, Curitiba, Campinas};
 
-        for (City city : cities) {
-            System.out.println("City name: " + city.getName() + "\nPopulation: " + city.getPopulation() + "\nArea: " + city.getArea() + "\nState: " + city.getState() + "\nIs capital? " + city.isCapital());
-            System.out.println("-------------------");
-        }
+    for (City city : cities) {
+        System.out.println("City name: " + city.getName() + "\nPopulation: " + city.getPopulation() + "\nArea: " + city.getArea() + "\nState: " + city.getState() + "\nIs capital? " + city.isCapital());
+        System.out.println("-------------------");
     }
 }
