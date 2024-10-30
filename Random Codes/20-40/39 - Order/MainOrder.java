b1353fda69a0cbbd288c560960c2280c640b068e
brunombs <brunozs@msn.com>
2024-10-30 18:23:24 -0300

    improving exercise

--- Random Codes/20-40/39 - Order/MainOrder.java
+++ Random Codes/20-40/39 - Order/MainOrder.java
@@ -1,8 +1,8 @@
 public class MainOrder {
     public static void main(String[] args) {
         RegularOrder reg1 = new RegularOrder(1, 30, 5);
-        RegularOrder reg2 = new RegularOrder(2, 4, 6);
-        ExpressOrder exp1 = new ExpressOrder(1, 45, 20, 2);
+        RegularOrder reg2 = new RegularOrder(2, 40, 5);
+        ExpressOrder exp1 = new ExpressOrder(1, 45, 2, 20);
         System.out.println(reg1);
         System.out.println(reg2);
         System.out.println(exp1);
