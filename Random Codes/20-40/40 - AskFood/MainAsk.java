9548d772ce80d3005fc3a0e74502f600d0a84a8e
brunombs <brunozs@msn.com>
2024-11-03 18:33:44 -0300

    improving

--- Random Codes/20-40/40 - AskFood/MainAsk.java
+++ Random Codes/20-40/40 - AskFood/MainAsk.java
@@ -3,6 +3,9 @@ public class MainAsk {
         AskFood hamburger = new AskFood("Hamburger", 300, 8.50);
         AskFood pizza = new AskFood("Pizza", 250, 10.00);
         AskFood salad = new AskFood("Salad", 150, 6.00);
+        AskBeverage cola = new AskBeverage("Coca-Cola", 150, 2.00);
+        AskBeverage orangeJuice = new AskBeverage("Orange Juice", 120, 3.50);
+        AskBeverage water = new AskBeverage("Water", 0, 1.00);
 
     }
 }
