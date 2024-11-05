6b44ece8ceef44c09bc63429967cad31aa5d895d
brunombs <brunozs@msn.com>
2024-11-04 23:52:59 -0300

    improving

--- Random Codes/41-60/41 - Phone/Smartphone.java
+++ Random Codes/41-60/41 - Phone/Smartphone.java
@@ -1,3 +1,17 @@
 public class Smartphone extends CellPhone {
+    private String os;
+    private int storage;
 
+    public Smartphone(String brand, String model, double batteryDuration, String os, int storage) {
+        super(brand, model, batteryDuration);
+        this.os = os;
+        this.storage = storage;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Operating System: " + os);
+        System.out.println("Storage: " + storage);
+    }
 }
