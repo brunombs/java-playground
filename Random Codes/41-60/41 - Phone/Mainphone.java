5c1107c8089ef9837fb95e7bc3fc23847243e223
brunombs <brunozs@msn.com>
2024-11-06 23:59:35 -0300

    improving

--- Random Codes/41-60/41 - Phone/Mainphone.java
+++ Random Codes/41-60/41 - Phone/Mainphone.java
@@ -1,4 +1,4 @@
-public static void main(String[] args) {
+public static void main(String[] ignoredArgs) {
     Scanner scanner = new Scanner(System.in);
     ArrayList<Cellphone> phones = new ArrayList<>();
 
