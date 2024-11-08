e7606bbb7d4680c3b3d0de361351f785fe2dd6a4
brunombs <brunozs@msn.com>
2024-11-08 05:59:19 -0300

    adding exercise

---
+++ Random Codes/41-60/42 - The Guess Game/TheGuessGame.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TheGuessGame {
+    private int numberToGuess;
+    private int numberOfTries;
+    private boolean hasWon;
+
+    public TheGuessGame() {
+        Random rand = new Random();
+        this.numberToGuess = rand.nextInt(100) + 1;
+        this.numberOfTries = 0;
+        this.hasWon = false;
+    }
+
+    public void play() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the guessing game");
+        System.out.println("Please enter your number to guess (between 0-100): ");
+
+        while (!hasWon) {
+            System.out.println("Write your guess");
+            int guess = input.nextInt();
+            numberOfTries++;
+
+            if (guess == numberToGuess) {
+                hasWon = true;
+                System.out.println("You guessed the number " + numberToGuess);
+                System.out.println("You used " + numberOfTries + " tries");
+            }
+            else if (guess < numberToGuess) {
+                System.out.println("Your guess is too low");
+            }
+            else {
+                System.out.println("Your guess is too high");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TheGuessGame game = new TheGuessGame();
+        game.play();
+    }
+}
