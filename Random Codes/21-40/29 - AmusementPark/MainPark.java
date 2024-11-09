cb65b4284d0184dcc27eebff0e0ad0f53c95aae5
brunombs <brunozs@msn.com>
2024-11-08 23:12:04 -0300

    organizing

--- Random Codes/21-40/29 - AmusementPark/MainPark.java
+++
@@ -1,8 +0,0 @@
-public class MainPark {
-    public static void main(String[] args) {
-        RollerCoaster rollerCoaster = new RollerCoaster("Dragon RollerCoaster", 50, 5, 100);
-        Carousel carousel = new Carousel("Lizard Carousel", 16, 3, 8);
-        rollerCoaster.displayInfo();
-        carousel.displayInfo();
-    }
-}
