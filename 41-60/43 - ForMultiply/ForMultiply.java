465764b41a5a8b4219bb8c81f8dbb3680aee343f
brunombs <brunozs@msn.com>
2024-11-14 23:21:51 -0300

    improving exercise

--- 41-60/43 - ForMultiply/ForMultiply.java
+++ 41-60/43 - ForMultiply/ForMultiply.java
@@ -1,13 +1,9 @@
-import java.util.Scanner;
-
-public class ForMultiply {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int number = sc.nextInt();
-        System.out.println("Multiplication table of the number: " + number);
-        for(int i = 1; i <= 10; i++){
-            System.out.println(number + " * " + i + " = " + number * i);
-        }
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int number = sc.nextInt();
+    System.out.println("Multiplication table of the number: " + number);
+    for (int i = 1; i <= 10; i++) {
+        System.out.println(number + " * " + i + " = " + number * i);
     }
 }
