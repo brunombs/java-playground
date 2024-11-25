044f675a91706d5fe24f9f8479cdb23e0cf7e031
brunombs <brunozs@msn.com>
2024-11-25 04:16:57 -0300

    improving exercise

--- 41-60/44 - Tibia/Main.java
+++ 41-60/44 - Tibia/Main.java
@@ -1,10 +1,3 @@
-// improvements:
-// set initial hp and mp 185/35 for level 8
-// ask level and define hp and mp for that level
-// ref: http://tutoriais.mundotibiabr.com/2008/04/tudo-sobre-vocaes.html
-// add oz capacity (initial = 470oz, mage+10, rp+20, ek+25)
-
-
 public static void main(String[] args) {
     Druid druid = new Druid(150, true);
     druid.printDetails();
@@ -20,4 +13,9 @@ public static void main(String[] args) {
     knight.printDetails();
     Knight knight2 = new Knight(8, false);
     knight2.printDetails();
+
+    Sorcerer sorcerer = new Sorcerer(150, true);
+    sorcerer.printDetails();
+    Sorcerer sorcerer2 = new Sorcerer(8, false);
+    sorcerer2.printDetails();
 }
--- 41-60/44 - Tibia/Sorcerer.java
+++ 41-60/44 - Tibia/Sorcerer.java
@@ -1,2 +1,28 @@
-public class Sorcerer {
+public class Sorcerer extends Vocation {
+
+    public Sorcerer(int level, boolean promoted) {
+        super(level, promoted);
+        this.vocation = promoted ? "Master Sorcerer" : "Sorcerer";
+    }
+
+    @Override
+    protected int calculateLifePoints(int level) {
+        int baseLifePoints = 185;
+        int lifeIncrement = 5;
+        return baseLifePoints + (level - 8) * lifeIncrement;
+    }
+
+    @Override
+    protected int calculateManaPoints(int level) {
+        int baseManaPoints = 35;
+        int manaIncrement = 30;
+        return baseManaPoints + (level - 8) * manaIncrement;
+    }
+
+    @Override
+    protected int calculateCapacity(int level) {
+        int baseCapacity = 470;
+        int capacityIncrement = 10;
+        return baseCapacity + (level - 8) * capacityIncrement;
+    }
 }
