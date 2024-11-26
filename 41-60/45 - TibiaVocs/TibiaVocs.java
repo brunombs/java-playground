ec56e10074cca996fad8be37a7868c9574d9de15
brunombs <brunozs@msn.com>
2024-11-26 03:29:10 -0300

    adding exercise

---
+++ 41-60/45 - TibiaVocs/TibiaVocs.java
@@ -0,0 +1,8 @@
+public static void main(String[] args) {
+    String[] vocs = new String[]{"Sorcerer", "Druid", "Paladin", "Knight"};
+    System.out.println(Arrays.toString(vocs));
+    System.out.println(vocs[0]);
+    for (String voc : vocs) {
+        System.out.println(voc);
+    }
+}
