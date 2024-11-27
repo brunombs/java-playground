1103b2518956de5e4148cbdbfaf703d5ac182e42
brunombs <brunozs@msn.com>
2024-11-27 04:49:02 -0300

    adding exercise

---
+++ 41-60/50 - Cities/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] cities = new String[3];
+        cities[0] = "San Francisco";
+        cities[1] = "Melbourne";
+        cities[2] = "New York";
+        System.out.println(cities[0]);
+        System.out.println(cities[1]);
+        System.out.println(cities[2]);
+    }
+}
