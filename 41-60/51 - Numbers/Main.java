90dc6cfb64f9f3965d20536f59d1cd76bcd19216
brunombs <brunozs@msn.com>
2024-11-27 04:51:08 -0300

    adding exercise

---
+++ 41-60/51 - Numbers/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 0;
+        numbers[1] = 1;
+        numbers[2] = 2;
+        numbers[3] = 3;
+        numbers[4] = 4;
+
+        for(int number : numbers) {
+            System.out.println(number);
+        }
+
+    }
+}
