a0e706ede4075ea2a8747d0f21a139acd2b067e3
brunombs <brunozs@msn.com>
2024-12-03 23:00:30 -0300

    improving exercise

--- 41-60/56 - Stock/Stock.java
+++ 41-60/56 - Stock/Stock.java
@@ -1,24 +1,12 @@
 public class Stock {
-    private String name;
-    private int quantity;
-    private double price;
+    private final String name;
+    private final int quantity;
+    private final double price;
 
     public Stock(String name, int quantity, double price) {
         this.name = name;
         this.quantity = quantity;
         this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public double getPrice() {
-        return price;
     }
 
     public void getDetails() {
