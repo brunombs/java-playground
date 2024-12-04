d2c74686a162afb869571e54be24c22f8fad97f8
brunombs <brunozs@msn.com>
2024-12-03 22:50:41 -0300

    improving exercise

--- 41-60/55 - City Guess/City.java
+++ 41-60/55 - City Guess/City.java
@@ -7,21 +7,31 @@ public class City {
         String[] cities = {"Salvador", "Rio de Janeiro", "Brasília", "São Paulo", "Porto Alegre", "Curitiba", "Maceió"};
         String myCity = cities[0];
         int attempts = 3;
-        System.out.println("Let's play a game!");
+
+        System.out.println("Let's play a guessing game!");
         System.out.println("""
                 Game rules:
-                ->You have 3 attempts\
-                
-                ->You gotta guess in which city the creator of this game was born.""");
-        System.out.println("Let's start it. Make a guess based on this list: " + Arrays.toString(cities));
+                -> You have 3 attempts.
+                -> You need to guess in which city the creator of this game was born.
+                """);
+        System.out.println("Here's the list of cities: " + Arrays.toString(cities));
+
         while (attempts > 0) {
+            System.out.print("Make a guess: ");
             String guess = scanner.nextLine();
-            if (guess.equals(myCity)) {
-                System.out.println("That's right :), you still had " + attempts + " attempts");
+
+            if (guess.equalsIgnoreCase(myCity)) {
+                System.out.println("Congratulations! You guessed it right! You still had " + attempts + " attempts remaining.");
                 break;
             } else {
-                System.out.println("Try again");
                 attempts--;
+                if (attempts == 1) {
+                    System.out.println("Try again! That's your last guess.");
+                } else if (attempts > 0) {
+                    System.out.println("Try again! You have " + attempts + " attempts remaining.");
+                } else {
+                    System.out.println("Sorry, you've run out of attempts. Game over!");
+                }
             }
         }
     }
