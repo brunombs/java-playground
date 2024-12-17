169d37af6ef0a5f92650e02146d958f631ba5d5e
brunombs <brunozs@msn.com>
2024-12-16 22:20:56 -0300

    adding exercise

---
+++ 41-60/60 - Vocations/Main.java
@@ -0,0 +1,28 @@
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String[]> content = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(Paths.get("41-60/vocations.txt"))) {  // Caminho relativo
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] tokens = line.split(",");
+                String vocation = tokens[0];
+                String promotedVocation = tokens[1];
+
+                content.add(new String[] {vocation, promotedVocation});
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        for (String[] pair : content) {
+            String vocation = pair[0];
+            String promotedVocation = pair[1];
+            System.out.println("Vocation: " + vocation.toUpperCase() + " and the promoted vocation: " + promotedVocation.toUpperCase());
+        }
+    }
+}
