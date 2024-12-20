17109713ee76b4ca4391c726333385d9a65d7dc6
brunombs <brunozs@msn.com>
2024-12-20 04:42:53 -0300

    improving exercise

--- 61-80/61 - Find Duplicates/FindDuplicates.java
+++ 61-80/61 - Find Duplicates/FindDuplicates.java
@@ -1,24 +1,33 @@
+import java.util.ArrayList;
+
 public class FindDuplicates {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 2, 3, 6};
         String[] array2 = {"A", "B", "C", "D", "E", "F", "F", "G", "G", "H"};
+        ArrayList<Integer> duplicate = new ArrayList<>();
+        ArrayList<String> duplicate2 = new ArrayList<>();
 
         System.out.println("Duplicate elements:");
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] == array[j]) {
-                    System.out.println(array[i]);
+                    duplicate.add(array[i]);
                 }
             }
         }
+
+        System.out.println(duplicate);
+
 
         System.out.println("Duplicate elements 2:");
         for (int i = 0; i < array2.length; i++) {
             for (int j = i + 1; j < array2.length; j++) {
                 if (array2[i].equals(array2[j])) {
-                    System.out.println(array2[i]);
+                    duplicate2.add(array2[i]);
                 }
             }
         }
+
+        System.out.println(duplicate2);
     }
 }
