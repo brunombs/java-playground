d605e12fe867676d4b22be8c05a921ebaa930de8
brunombs <brunozs@msn.com>
2025-01-23 23:51:45 -0300

    improving exercise

--- 61-80/64 - Counter Strike/Main.java
+++ 61-80/64 - Counter Strike/Main.java
@@ -3,17 +3,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.println("Would you like to play as a CT or Terror?");
         String team = input.nextLine();
-        if (team.equalsIgnoreCase("CT")) {
-            System.out.println("You're playing as CT");
-        } else if (team.equalsIgnoreCase("Terror")) {
-            System.out.println("You're playing as Terror");
-        } else {
+        while (!team.equalsIgnoreCase("CT") && !team.equalsIgnoreCase("Terror")) {
             System.out.println("Invalid option. Please choose either CT or Terror.");
+            team = input.nextLine();
+        }
+        System.out.println("You're playing as " + team);
+
+        int ctPoints = 0;
+        int terrorPoints = 0;
+        int round = 1;
+
+        while (round <= 30 && ctPoints < 16 && terrorPoints < 16) {
+            System.out.println("\nRound " + round + " - CT: " + ctPoints + " | Terror: " + terrorPoints);
+            System.out.println("Who won this round? (CT or Terror)");
+            String roundWinner = input.nextLine();
+
+            if (roundWinner.equalsIgnoreCase("CT")) {
+                ctPoints++;
+            } else if (roundWinner.equalsIgnoreCase("Terror")) {
+                terrorPoints++;
+            } else {
+                System.out.println("Invalid option. Please choose either CT or Terror.");
+                continue;
+            }
+
+            round++;
         }
 
-        System.out.println("Would you like to play against real players or bots?");
-        String oponentTeam = input.nextLine();
+        System.out.println("\nFinal Score - CT: " + ctPoints + " | Terror: " + terrorPoints);
+        if (ctPoints == 16) {
+            System.out.println("CT Wins!");
+        } else if (terrorPoints == 16) {
+            System.out.println("Terror Wins!");
+        } else {
+            System.out.println("It's a draw!");
+        }
     }
 }
