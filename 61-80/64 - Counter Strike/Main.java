f0d60daedd68648ac81f6ba8e4d1a8ac16af940d
brunombs <brunozs@msn.com>
2025-01-24 23:43:13 -0300

    improving exercise

--- 61-80/64 - Counter Strike/Main.java
+++ 61-80/64 - Counter Strike/Main.java
@@ -16,18 +16,24 @@ public class Main {
         int terrorPoints = 0;
         int round = 1;
 
+        System.out.println("Is the first round a knife round? (yes/no)");
+        String knifeRound = input.nextLine();
+        while (!knifeRound.equalsIgnoreCase("yes") && !knifeRound.equalsIgnoreCase("no")) {
+            System.out.println("Invalid option. Please choose either yes or no.");
+            knifeRound = input.nextLine();
+        }
+
+        if (knifeRound.equalsIgnoreCase("yes")) {
+            System.out.println("The first round is a knife round! Only knives are allowed.");
+        } else {
+            System.out.println("The first round is a normal round.");
+        }
+
         while (round <= 30 && ctPoints < 16 && terrorPoints < 16) {
             System.out.println("\nRound " + round + " - CT: " + ctPoints + " | Terror: " + terrorPoints);
-            System.out.println("Is this a knife round or a normal round? (knife/normal)");
-            String roundType = input.nextLine();
 
-            while (!roundType.equalsIgnoreCase("knife") && !roundType.equalsIgnoreCase("normal")) {
-                System.out.println("Invalid option. Please choose either knife or normal.");
-                roundType = input.nextLine();
-            }
-
-            if (roundType.equalsIgnoreCase("knife")) {
-                System.out.println("This is a knife round! Only knives are allowed.");
+            if (round == 1 && knifeRound.equalsIgnoreCase("yes")) {
+                System.out.println("This is a knife round!");
             } else {
                 System.out.println("This is a normal round.");
             }
