177c61777fff4a66ae4961673eb77dbc46f09189
brunombs <brunozs@msn.com>
2025-01-24 23:42:01 -0300

    improving exercise

--- 61-80/64 - Counter Strike/Main.java
+++ 61-80/64 - Counter Strike/Main.java
@@ -18,6 +18,20 @@ public class Main {
 
         while (round <= 30 && ctPoints < 16 && terrorPoints < 16) {
             System.out.println("\nRound " + round + " - CT: " + ctPoints + " | Terror: " + terrorPoints);
+            System.out.println("Is this a knife round or a normal round? (knife/normal)");
+            String roundType = input.nextLine();
+
+            while (!roundType.equalsIgnoreCase("knife") && !roundType.equalsIgnoreCase("normal")) {
+                System.out.println("Invalid option. Please choose either knife or normal.");
+                roundType = input.nextLine();
+            }
+
+            if (roundType.equalsIgnoreCase("knife")) {
+                System.out.println("This is a knife round! Only knives are allowed.");
+            } else {
+                System.out.println("This is a normal round.");
+            }
+
             System.out.println("Who won this round? (CT or Terror)");
             String roundWinner = input.nextLine();
 
